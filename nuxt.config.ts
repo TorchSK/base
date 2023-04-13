60b35e3b3a7bece483e1f283ed1887282db317dc
TorchSK <gtorch@gmail.com>
2023-04-13 14:51:54 +0200

    dd

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,19 +8,6 @@ export default defineNuxtConfig({
         '@nuxtjs/web-vitals'
     ],
 
- 
-    unocss: {
-        preflight: true,
-        wind: true,
-        icons: true,
-        shortcuts: [],
-        rules: [],
-
-        safelist: [
-            'bg-black','bg-blue-300','bg-red-300','bg-green-300','bg-gray-300','bg-orange-300', 'bg-white', 'text-black',
-            'text-blue-300','text-red-300','text-green-300','text-gray-300','text-orange-300', 'text-white'
-        ]
-    },
 
     build: {
         transpile: [
