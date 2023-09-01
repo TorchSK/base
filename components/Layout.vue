d340ddfd93bc9008ca11527386b86e9509cd833a
TorchSK <gtorch@gmail.com>
2023-09-01 20:41:16 +0200

    dd

--- components/Layout.vue
+++ components/Layout.vue
@@ -1,9 +1,8 @@
 <template>
 
 <div class="flex-grow space-y-12 lg:space-y-24 p-2">
-    
+        
     <template v-for="element in layout.elements">
-
         <component :is="componentsMap[element.type]" :element="element" />
 
     </template>
