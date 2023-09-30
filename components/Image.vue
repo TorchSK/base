b30635ad3f963f3e277575f6a6e55d1314142d77
TorchSK <gtorch@gmail.com>
2023-09-30 20:08:13 +0200

    dd

---
+++ components/Image.vue
@@ -0,0 +1,38 @@
+<template>
+    
+    <nuxt-img v-if="cdn" :src="src" class="max-w-full max-h-full overflow-hidden" />
+    
+    <img v-else :src="src" />
+
+</template>
+
+<script setup>
+ 
+const props = defineProps({
+    image: {
+        type: Object,
+        required: false
+    },
+    type: {
+        type: String,
+        default: "public"
+    },
+    cdn: {
+        type: Boolean,
+        default: true
+    }
+})
+
+let src
+
+if(props.image && props.image.cloud_id)
+{
+    src = props.image.cloud_id
+}
+else
+{
+    src="placeholder.webp"
+}
+
+
+</script>
