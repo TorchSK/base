a9620da4c25d93926dbe6efbc854434c73b7aadc
TorchSK <gtorch@gmail.com>
2023-10-20 09:17:47 +0200

    ff

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,6 @@ export default defineNuxtConfig({
 
     modules: [
         '@unocss/nuxt',
-        '@vueuse/nuxt',
-        '@nuxtjs/device',
-        '@nuxt/image',
-        '@nuxtjs/web-vitals'
-    ],
+    ]
 
 })
